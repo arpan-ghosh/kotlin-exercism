8426db5f89cd11ee61b2195957094f54def417f2
Arpan Ghosh <aghosh@tenable.com>
2021-10-24 23:12:51 -0400

    matrix column and row get and set, immutable lists

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,14 +1,47 @@
 class Matrix(private val matrixAsString: String) {
 
-    init {
+    var STRING_MATRIX : MutableList<String> = arrayListOf<String>()
+    var INT_MATRIX : MutableList<MutableList<Int>> = arrayListOf<MutableList<Int>>()
+    var LINES : List<String>
+    var NUM_ROWS : Int = 0
+    var NUM_COLS : Int = 0
 
+    init {
+        LINES = matrixAsString.split(java.lang.System.lineSeparator())
+        NUM_ROWS = LINES.size
+        this.cleanLines(LINES)
+    }
+
+    fun cleanLines(originalMatrix: List<String>) {
+        for (row in originalMatrix) {
+            var newRow = row.replace("\\s{2,}".toRegex(), " ").trim()
+            newRow = newRow.replace(" ",",")
+
+            var numbers : List<String> = newRow.split(",")
+            var numbersInt : MutableList<Int> = arrayListOf<Int>()
+            NUM_COLS = numbers.size
+
+            for (number in numbers) {
+                numbersInt.add(number.toInt())
+            }
+
+            INT_MATRIX.add(numbersInt)
+        }
+
+        return;
     }
 
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        var col : MutableList<Int> = arrayListOf<Int>()
+
+        for (row in INT_MATRIX) {
+            col.add(row.get(colNr-1))
+        }
+
+        return col
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return INT_MATRIX[rowNr-1]
     }
 }
